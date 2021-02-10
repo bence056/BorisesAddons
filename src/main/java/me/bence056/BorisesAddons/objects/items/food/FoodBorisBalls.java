4b2f4c07dc4efa4f831d5f5ff7b8c5d352b1cc98
bence056 <vargabencecsaba2005@gmail.com>
2021-02-10 12:56:22 +0100

    removed RightClick Event from BorisBalls

--- src/main/java/me/bence056/BorisesAddons/objects/items/food/FoodBorisBalls.java
+++ src/main/java/me/bence056/BorisesAddons/objects/items/food/FoodBorisBalls.java
@@ -2,14 +2,17 @@ package me.bence056.BorisesAddons.objects.items.food;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.PotionTypes;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.asm.transformers.PotionEffectTransformer;
 
 public class FoodBorisBalls extends FoodBase {
 
@@ -23,27 +26,5 @@ public class FoodBorisBalls extends FoodBase {
     protected void onFoodEaten(ItemStack foodItem, World world, EntityPlayer player) {
         super.onFoodEaten(foodItem, world, player);
         player.addPotionEffect(new PotionEffect(Potion.getPotionById(8),200, 5));
-    }
-
-    @Override
-    public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer player, EntityLivingBase target, EnumHand hand) {
-
-        World world = player.getEntityWorld();
-        if(world.isRemote) {
-            if(true/*target instanceof EntityPlayer*/) { //TODO
-                try{
-
-                    player.sendMessage(new TextComponentString("\u00A75Don't put Boris's balls under someone's nose..."));
-                    player.playSound(SoundEvents.ENTITY_HORSE_BREATHE, 10, 1);
-                    target.addPotionEffect(new PotionEffect(Potion.getPotionById(9), 100, 5));
-                    stack.shrink(1);
-
-                }catch (Exception ex) {
-                    System.out.println("Something happened - " + ex.getStackTrace());
-                }
-            }
-        }
-
-        return super.itemInteractionForEntity(stack, player, target, hand);
     }
 }
